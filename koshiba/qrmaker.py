65a6ab199f481798989862e7c1ac7e1d79051bfd
RikuKoshiba <koshiba.riku.ig@mynavi.jp>
2023-05-29 08:28:47 +0000

    202305291728

--- koshiba/qrmaker.py
+++ koshiba/qrmaker.py
@@ -4,16 +4,15 @@ import qrcode
 # qrlistのファイルを開く
 # qr_list = open('qr_list.txt', 'r')
 
-# 個数計算
+
 count = 1
+with open('qrlist.txt', encoding="utf-8") as f:
+    for i in f:
+        print(i)
+        # ファイルの保存場所・名前
+        path = os.path.join("../../..", "output", f"{count}.png")
 
-with open('qr_list.txt', encoding="utf-8") as f:
-    for line in f:
-       print(line)
-# for i in range(1, count+1):
-    # # ファイルの保存場所・名前
-    # path = os.path.join("../../../files", "output", f"{i}.png")
-
-    # # pngファイルの生成
-    # img = qrcode.make('qrlist')
-    # img.save(path)
+        # pngファイルの生成
+        img = qrcode.make(i)
+        img.save(path)
+        count += 1
